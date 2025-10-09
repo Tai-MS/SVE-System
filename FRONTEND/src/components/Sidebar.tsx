2feaa777c42deaae243fb87eff392c7b0150c25e
alejo <alejoviviani12@gmail.com>
2025-10-09 18:23:59 -0300

    asd

--- FRONTEND/src/components/Sidebar.tsx
+++ FRONTEND/src/components/Sidebar.tsx
@@ -19,10 +19,10 @@ const Sidebar = ({ links }) => {
 
   return (
     <>
-      <div className="fixed mt-8 h-full bg-white shadow-md p-10 transition-all duration-500 z-40 shadow-gray-400">
+      <div className="flex mt-8 bg-white shadow-md p-10 transition-all duration-500 ">
         <nav className="space-y-10">
           {links.map((link: Link, index: number) => {
-            if (link.rol && link.rol.includes(usuarioRol as string)) {
+            if (link.rol.includes(usuarioRol as string)) {
               return (
                 <NavLink
                   key={index}
@@ -38,6 +38,73 @@ const Sidebar = ({ links }) => {
           })}
         </nav>
       </div>
+      {/* Sidebar
+      <aside className="w-60 bg-purple-50 p-4 flex flex-col gap-2">
+        <img
+          src="/logoterciario.png"
+          alt="Logo"
+          className="w-12 h-12 mb-4 mx-auto"
+        />
+        <List className="text-left cursor-pointer px-3 py-2 rounded-md font-medium">
+          <ListItem disablePadding>
+            <ListItemButton selected>
+              <ListItemText
+                primary="Anuncios"
+                className={"bg-white text-purple-600 shadow-md"}
+              />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton onClick={() => navigate("/carreras")}>
+              <ListItemText
+                primary="Carreras"
+                className={"text-gray-700 hover:bg-purple-100"}
+              />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText
+                primary="Alumnos"
+                className={"text-gray-700 hover:bg-purple-100"}
+              />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton onClick={() => setOpen(!open)}>
+              <ListItemText
+                primary="Mensajes"
+                className={"text-gray-700 hover:bg-purple-100"}
+              />
+              {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+          </ListItem>
+
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              <ListItem disablePadding>
+                <ListItemButton sx={{ pl: 4 }}>
+                  <ListItemText
+                    primary="Recibidos"
+                    className={"text-gray-700 hover:bg-purple-100"}
+                  />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton sx={{ pl: 4 }}>
+                  <ListItemText
+                    primary="Enviados"
+                    className={"text-gray-700 hover:bg-purple-100"}
+                  />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </Collapse>
+        </List>
+      </aside> */}
     </>
   );
 };
